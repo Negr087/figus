c0b311af6e5a48283d668562d9c7321b78759156
Negr087 <spibanezpaypal@gmail.com>
2026-06-12 19:36:39 +0000

    fix(my-stickers): clarify bulk publish button label

--- src/components/MyStickers.tsx
+++ src/components/MyStickers.tsx
@@ -178,7 +178,7 @@ export function MyStickers({
                         flexShrink: 0,
                       }}
                     >
-                      ⚡ {nums.length}
+                      ⚡ PUBLICAR {nums.length}
                     </button>
                   </div>
                 );
